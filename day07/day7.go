f228674754cab290322ed02a9ff2904183475ff3
Tadas Maslauskas <tadas.maslauskas@kilo.health>
2022-12-07 22:18:55 +0200

    day 7: simple dir with size

--- day07/day7.go
+++ day07/day7.go
@@ -1,1 +1,25 @@
 package day07
+
+type Directory struct {
+	Name  string
+	Files map[string]int
+}
+
+func (d Directory) Size() int {
+	sum := 0
+	for _, size := range d.Files {
+		sum += size
+	}
+	return sum
+}
+
+func (d Directory) AddFile(name string, size int) {
+	d.Files[name] = size
+}
+
+func MakeDir(name string) Directory {
+	return Directory{
+		name,
+		map[string]int{},
+	}
+}
--- day07/day7_test.go
+++ day07/day7_test.go
@@ -5,5 +5,24 @@ import (
 )
 
 func TestExample(t *testing.T) {
+	t.Run("directory can have a size", func(t *testing.T) {
+		dir := MakeDir("x")
+		size := dir.Size()
 
+		if size != 0 {
+			t.Errorf("expected size to be 0, got %d", size)
+		}
+	})
+
+	t.Run("directory size includes files size within it", func(t *testing.T) {
+		dir := MakeDir("x")
+		dir.AddFile("a.txt", 100)
+
+		size := dir.Size()
+		want := 100
+
+		if size != want {
+			t.Errorf("expected size to be %d, got %d", want, size)
+		}
+	})
 }
