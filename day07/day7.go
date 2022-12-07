6d0dc4e9fe8db355baf3fb1d17e093ca1e83e903
Tadas Maslauskas <tadas.maslauskas@kilo.health>
2022-12-07 10:06:04 +0200

    day 7: create file

--- day07/day7.go
+++ day07/day7.go
@@ -4,10 +4,20 @@ type Folder struct {
 	Name string
 }
 
+type File struct {
+	Name string
+	Size int
+}
+
 type RootDir struct {
 	Folders []Folder
+	Files   []File
 }
 
 func (r *RootDir) addFolder(name string) {
 	r.Folders = append(r.Folders, Folder{name})
+}
+
+func (r *RootDir) addFile(name string, size int) {
+	r.Files = append(r.Files, File{name, size})
 }
--- day07/day7_test.go
+++ day07/day7_test.go
@@ -2,6 +2,13 @@ package day07
 
 import "testing"
 
+// + create folder
+// create file
+// get file size
+// create file inside folder and get its size
+// create folder with subfolders and get its size
+// select folder
+
 func TestExample(t *testing.T) {
 	t.Run("will create folder within root", func(t *testing.T) {
 		root := RootDir{}
@@ -12,6 +19,18 @@ func TestExample(t *testing.T) {
 
 		if want != got {
 			t.Errorf("expected dir count to be %d, got %d", want, got)
+		}
+	})
+
+	t.Run("will create file within root", func(t *testing.T) {
+		root := RootDir{}
+		root.addFile("a", 123)
+
+		want := 1
+		got := len(root.Files)
+
+		if want != got {
+			t.Errorf("expected file count to be %d, got %d", want, got)
 		}
 	})
 }
