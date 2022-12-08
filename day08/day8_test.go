4d94c38eb156f3aa76da6c246121d48a1954f084
Tadas Maslauskas <tadas.maslauskas@kilo.health>
2022-12-08 10:37:13 +0200

    day 8: failing test was due to wrong assumption

--- day08/day8_test.go
+++ day08/day8_test.go
@@ -50,7 +50,7 @@ func TestCheckBlockingTrees(t *testing.T) {
 	t.Run("middle tree unblocked from top", func(t *testing.T) {
 		board := Board{
 			[]int{1, 0, 1},
-			[]int{1, 0, 1},
+			[]int{1, 1, 1},
 			[]int{1, 1, 1},
 		}
 
@@ -64,7 +64,7 @@ func TestCheckBlockingTrees(t *testing.T) {
 	t.Run("middle tree unblocked from right", func(t *testing.T) {
 		board := Board{
 			[]int{1, 1, 1},
-			[]int{1, 0, 0},
+			[]int{1, 1, 0},
 			[]int{1, 1, 1},
 		}
 
@@ -78,7 +78,7 @@ func TestCheckBlockingTrees(t *testing.T) {
 	t.Run("middle tree unblocked from bottom", func(t *testing.T) {
 		board := Board{
 			[]int{1, 1, 1},
-			[]int{1, 0, 1},
+			[]int{1, 1, 1},
 			[]int{1, 0, 1},
 		}
 
@@ -92,7 +92,7 @@ func TestCheckBlockingTrees(t *testing.T) {
 	t.Run("middle tree unblocked from left", func(t *testing.T) {
 		board := Board{
 			[]int{1, 1, 1},
-			[]int{0, 0, 1},
+			[]int{0, 1, 1},
 			[]int{1, 1, 1},
 		}
 
