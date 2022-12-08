2e467f5c25b7548f07fb74dff7d13d24c63579a9
Tadas Maslauskas <tadas.maslauskas@kilo.health>
2022-12-08 09:21:59 +0200

    day 8: check blocked tree by coords from all sides

--- day08/day8.go
+++ day08/day8.go
@@ -19,3 +19,64 @@ func BuildBoard(input []string) Board {
 }
 
 type Board [][]int
+
+func (b Board) CheckBlocked(x int, y int) bool {
+	top := CheckTop(b, x, y)
+	right := CheckRight(b, x, y)
+	bottom := CheckBottom(b, x, y)
+	left := CheckLeft(b, x, y)
+
+	return top && right && bottom && left
+}
+
+func CheckRight(b Board, x int, y int) bool {
+	val := b[y][x]
+
+	for i := x + 1; i < len(b[y]); i++ {
+		neighbor := b[y][i]
+		if neighbor > val {
+			return true
+		}
+	}
+
+	return false
+}
+
+func CheckLeft(b Board, x int, y int) bool {
+	val := b[y][x]
+
+	for i := x - 1; i >= 0; i-- {
+		neighbor := b[y][i]
+		if neighbor > val {
+			return true
+		}
+	}
+
+	return false
+}
+
+func CheckTop(b Board, x int, y int) bool {
+	val := b[y][x]
+
+	for i := y - 1; i >= 0; i-- {
+		neighbor := b[i][x]
+		if neighbor > val {
+			return true
+		}
+	}
+
+	return false
+}
+
+func CheckBottom(b Board, x int, y int) bool {
+	val := b[y][x]
+
+	for i := y + 1; i < len(b); i++ {
+		neighbor := b[i][x]
+		if neighbor > val {
+			return true
+		}
+	}
+
+	return false
+}
--- day08/day8_test.go
+++ day08/day8_test.go
@@ -17,3 +17,94 @@ func TestBuildBoard(t *testing.T) {
 		t.Errorf("got %v, expected %v", board, want)
 	}
 }
+
+func TestCheckBlockingTrees(t *testing.T) {
+	t.Run("corner tree", func(t *testing.T) {
+		board := Board{
+			[]int{0, 1},
+			[]int{1, 0},
+		}
+
+		got := board.CheckBlocked(0, 0)
+		want := false
+
+		if want != got {
+			t.Errorf("expected result to be %v, got %v", want, got)
+		}
+	})
+
+	t.Run("middle tree blocked", func(t *testing.T) {
+		board := Board{
+			[]int{1, 1, 1},
+			[]int{1, 0, 1},
+			[]int{1, 1, 1},
+		}
+
+		got := board.CheckBlocked(1, 1)
+		want := true
+
+		if want != got {
+			t.Errorf("expected result to be %v, got %v", want, got)
+		}
+	})
+
+	t.Run("middle tree unblocked from top", func(t *testing.T) {
+		board := Board{
+			[]int{1, 0, 1},
+			[]int{1, 0, 1},
+			[]int{1, 1, 1},
+		}
+
+		got := board.CheckBlocked(1, 1)
+		want := false
+
+		if want != got {
+			t.Errorf("expected result to be %v, got %v", want, got)
+		}
+	})
+
+	t.Run("middle tree unblocked from right", func(t *testing.T) {
+		board := Board{
+			[]int{1, 1, 1},
+			[]int{1, 0, 0},
+			[]int{1, 1, 1},
+		}
+
+		got := board.CheckBlocked(1, 1)
+		want := false
+
+		if want != got {
+			t.Errorf("expected result to be %v, got %v", want, got)
+		}
+	})
+
+	t.Run("middle tree unblocked from bottom", func(t *testing.T) {
+		board := Board{
+			[]int{1, 1, 1},
+			[]int{1, 0, 1},
+			[]int{1, 0, 1},
+		}
+
+		got := board.CheckBlocked(1, 1)
+		want := false
+
+		if want != got {
+			t.Errorf("expected result to be %v, got %v", want, got)
+		}
+	})
+
+	t.Run("middle tree unblocked from left", func(t *testing.T) {
+		board := Board{
+			[]int{1, 1, 1},
+			[]int{0, 0, 1},
+			[]int{1, 1, 1},
+		}
+
+		got := board.CheckBlocked(1, 1)
+		want := false
+
+		if want != got {
+			t.Errorf("expected result to be %v, got %v", want, got)
+		}
+	})
+}
