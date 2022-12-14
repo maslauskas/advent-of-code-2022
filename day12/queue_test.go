a43a70255e8d374e0122f62eccc4edb39c1337bc
Tadas Maslauskas <tadas.maslauskas@kilo.health>
2022-12-14 12:09:50 +0200

    day12: new queue implementation

--- day12/queue_test.go
+++ day12/queue_test.go
@@ -1,14 +1,34 @@
 package day12
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 type Queue struct {
 	Elements []QueueElement
 }
 
-func (q *Queue) Push(element QueueElement, priority int) {
+func (q *Queue) Push(value interface{}, priority int) {
 	// less priority is better
-	q.Elements = append(q.Elements, element)
+	newElement := QueueElement{value, priority}
+	if q.Len() == 0 {
+		q.Elements = append(q.Elements, newElement)
+		return
+	}
+
+	q.Elements = append(q.Elements, newElement)
+
+	i := q.Len() - 2
+	for i >= 0 {
+		if q.Elements[i].Priority > priority {
+			q.Elements[i+1] = q.Elements[i]
+			i--
+		} else {
+			break
+		}
+	}
+	q.Elements[i+1] = newElement
 }
 
 func (q *Queue) Len() int {
@@ -16,14 +36,15 @@ func (q *Queue) Len() int {
 }
 
 func (q *Queue) Pop() QueueElement {
-	elem := q.Elements[q.Len()-1]
-	q.Elements = q.Elements[:q.Len()-1]
+	elem := q.Elements[0]
+	q.Elements = q.Elements[1:]
 
 	return elem
 }
 
 type QueueElement struct {
-	Coords []Coord
+	Value    interface{}
+	Priority int
 }
 
 func TestPriorityQueue(t *testing.T) {
@@ -35,9 +56,9 @@ func TestPriorityQueue(t *testing.T) {
 
 	t.Run("will dequeue one by one", func(t *testing.T) {
 		queue := Queue{}
-		queue.Push(QueueElement{[]Coord{{0, 0}}}, 5)
-		queue.Push(QueueElement{[]Coord{{1, 1}}}, 1)
-		queue.Push(QueueElement{[]Coord{{2, 2}}}, 3)
+		queue.Push([]Coord{{0, 0}}, 5)
+		queue.Push([]Coord{{1, 1}}, 1)
+		queue.Push([]Coord{{2, 2}}, 3)
 
 		queue.Pop()
 		AssertQueueLength(t, queue, 2)
@@ -48,6 +69,25 @@ func TestPriorityQueue(t *testing.T) {
 		queue.Pop()
 		AssertQueueLength(t, queue, 0)
 	})
+
+	t.Run("will dequeue in the right order", func(t *testing.T) {
+		queue := Queue{}
+		wantEl3 := []Coord{{0, 0}}
+		queue.Push(wantEl3, 5)
+		wantEl1 := []Coord{{1, 1}}
+		queue.Push(wantEl1, 1)
+		wantEl2 := []Coord{{2, 2}}
+		queue.Push(wantEl2, 3)
+
+		el1 := queue.Pop()
+		AssertQueueElement(t, el1, QueueElement{wantEl1, 1})
+	})
+}
+
+func AssertQueueElement(t *testing.T, got QueueElement, want QueueElement) {
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("expected element to be %v, got %v", want, got)
+	}
 }
 
 func AssertQueueLength(t *testing.T, queue Queue, want int) {
