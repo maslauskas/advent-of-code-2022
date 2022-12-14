85444ed223a342dd634883c8d33a64af1c573528
Tadas Maslauskas <tadas.maslauskas@kilo.health>
2022-12-14 11:52:56 +0200

    day12: new queue implementation

--- day12/queue_test.go
+++ day12/queue_test.go
@@ -15,19 +15,46 @@ func (q *Queue) Len() int {
 	return len(q.Elements)
 }
 
+func (q *Queue) Pop() QueueElement {
+	elem := q.Elements[q.Len()-1]
+	q.Elements = q.Elements[:q.Len()-1]
+
+	return elem
+}
+
 type QueueElement struct {
+	Coords []Coord
 }
 
 func TestPriorityQueue(t *testing.T) {
 	t.Run("will add element", func(t *testing.T) {
 		queue := Queue{}
 		queue.Push(QueueElement{}, 1)
-
-		want := 1
-		got := queue.Len()
-
-		if want != got {
-			t.Errorf("expected queue size of %d, got %d", want, got)
-		}
+		AssertQueueLength(t, queue, 1)
 	})
+
+	t.Run("will dequeue one by one", func(t *testing.T) {
+		queue := Queue{}
+		queue.Push(QueueElement{[]Coord{{0, 0}}}, 5)
+		queue.Push(QueueElement{[]Coord{{1, 1}}}, 1)
+		queue.Push(QueueElement{[]Coord{{2, 2}}}, 3)
+
+		queue.Pop()
+		AssertQueueLength(t, queue, 2)
+
+		queue.Pop()
+		AssertQueueLength(t, queue, 1)
+
+		queue.Pop()
+		AssertQueueLength(t, queue, 0)
+	})
+}
+
+func AssertQueueLength(t *testing.T, queue Queue, want int) {
+	t.Helper()
+	got := queue.Len()
+
+	if want != got {
+		t.Errorf("expected queue size of %d, got %d", want, got)
+	}
 }
